93c6a5b90aa0d1f5520c55be7b1080b2f613e381
olga-nest <olga.w.nesterova@gmail.com>
2017-11-12 17:09:02 -0800

    Add UIAlert to save new room name

--- FurnitureTracker/MasterViewController.swift
+++ FurnitureTracker/MasterViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Realm
 
 class MasterViewController: UITableViewController {
 
@@ -16,10 +17,12 @@ class MasterViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let realm = try! Realm()
+        
         navigationItem.leftBarButtonItem = editButtonItem
 
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addRoom(_:)))
         navigationItem.rightBarButtonItem = addButton
         if let split = splitViewController {
             let controllers = split.viewControllers
@@ -38,10 +41,32 @@ class MasterViewController: UITableViewController {
     }
 
     @objc
-    func insertNewObject(_ sender: Any) {
-        objects.insert(NSDate(), at: 0)
+    func addRoom(_ sender: Any) {
+        let alert = UIAlertController(title: "Add new room",
+                                      message: nil,
+                                      preferredStyle: .alert)
+        alert.addTextField {(textField : UITextField!) -> Void in
+            textField.placeholder = "Enter Room Name"
+        }
+        let okAction = UIAlertAction(title: "Save", style: .default) {
+            (action: UIAlertAction) in
+            
+            if  let alertTextField = alert.textFields?.first, alertTextField.text != nil {
+                let room = Room()
+                room.name = alertTextField.text!
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+        
         let indexPath = IndexPath(row: 0, section: 0)
-        tableView.insertRows(at: [indexPath], with: .automatic)
+        
+    //    tableView.insertRows(at: [indexPath], with: .automatic)
+        
+        
     }
 
     // MARK: - Segues
